41f758f706c44708aac51134d481c1a1057769cc
Pathrikar Atul <Pathrikaratul7@gmail.com>
2026-07-26 15:50:14 +0530

    Add project files.

---
+++ Interview_prep_8777/abs_interfaceDay1.cs
@@ -0,0 +1,91 @@
+﻿
+
+public class abs_interfaceDay1
+{
+
+
+    // bank account Saving account & currentAccount
+    // both account having check balance , withdrwal , deposit options
+    // calculate interest option available only for saving account
+
+    public abstract class BankService
+    {
+        public double Balance = 10000;
+        public void CheckBalance() // we shared this common logic everywere (Balance)
+        {
+           Console.WriteLine($"Your balance is : { Balance}");
+        }
+        public abstract void withdraw(double amount);
+        public abstract void deposit(double amount);
+    
+    }
+    public interface IinterestCalculater
+    { 
+      double calculateinterest();
+    }
+    public class SavingAccount : BankService , IinterestCalculater
+    {
+        public override void withdraw(double amount)
+        {
+            Balance -= amount;
+        }
+        public override void deposit(double amount)
+        {
+            Balance += amount;
+        }
+        public double calculateinterest()
+        {
+            return Balance * 5;
+        }
+    }
+    public class currentAccount : BankService
+    {
+        public override void withdraw(double amount)
+        {
+            Balance -= amount;
+        }
+        public override void deposit(double amount)
+        {
+            Balance += amount;
+        }
+
+    }
+
+    static void Main(string[] args)
+
+    {
+        SavingAccount savacc = new SavingAccount();
+
+        Console.WriteLine("Saving Account Details");
+        savacc.CheckBalance();
+        savacc.deposit(500);
+        Console.WriteLine("New balance updated, please see below");
+        savacc.CheckBalance();
+        Console.WriteLine("New balance updated, please see below");
+        savacc.withdraw(500);
+        savacc.CheckBalance();
+        Console.WriteLine("New balance updated, please see below");
+
+        savacc.calculateinterest();
+
+        Console.WriteLine("Current Account Details");
+        currentAccount cuacc = new currentAccount();
+        cuacc.CheckBalance();
+        Console.WriteLine("New balance updated, please see below");
+        cuacc.deposit(1000);
+        Console.WriteLine("New balance updated, please see below");
+        cuacc.CheckBalance();
+        Console.WriteLine("New balance updated, please see below");
+        cuacc.withdraw(700);
+        cuacc.CheckBalance();
+
+        Console.WriteLine("New balance updated, please see below");
+
+
+
+
+
+
+    }
+
+}
