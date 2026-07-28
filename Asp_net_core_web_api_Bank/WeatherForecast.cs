4026cec3ff53ff3bf6c1261430c9dec4e527d771
Pathrikar Atul <Pathrikaratul7@gmail.com>
2026-07-28 14:21:02 +0530

    ASP_net_Core_project_added

---
+++ Asp_net_core_web_api_Bank/WeatherForecast.cs
@@ -0,0 +1,13 @@
+namespace Asp_net_core_web_api_Bank
+{
+    public class WeatherForecast
+    {
+        public DateOnly Date { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+        public string? Summary { get; set; }
+    }
+}
