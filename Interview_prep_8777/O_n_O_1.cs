7ffddf7d63ba6b406be748cfac472a53fbcfb5bb
Pathrikar Atul <Pathrikaratul7@gmail.com>
2026-08-10 16:19:34 +0530

    C# interview question and answers

--- Interview_prep_8777/O_n_O_1.cs
+++ Interview_prep_8777/O_n_O_1.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -9,11 +10,114 @@ namespace Interview_prep_8777
     public class O_n_O_1
     {
 
-       
+        public record Employee(string Name, int Age);
+        public record EmployeeDto(int Id,string Name,decimal Salary);
+        #region Reverse a string without using Reverse()
+        public static void Reverse(string txtvalue)
+        {
+            string rev = string.Empty;
+            if (!string.IsNullOrEmpty(txtvalue))
+            {
+                for (int i = 0; i < txtvalue.Length; i++)
+                {
+                    rev = txtvalue[i] + rev;
+                }
+                Console.WriteLine($"Reverce string : {rev}");
+
+
+                StringBuilder sb = new StringBuilder();
+                for (int i = txtvalue.Length - 1; i >= 0; i--)
+                {
+                    sb.Append(txtvalue[i]);
+
+                }
+                Console.WriteLine($"Reverce string using StringBuilder : {sb.ToString()}");
+            }
+
+
+            
+
+        }
+        #endregion
+
+        #region Find duplicate characters in a string.
+        public static void FindDuplicate(string txtvalue)
+        {
+           
+
+            for (int i = 0; i < txtvalue.Length; i++)
+            {
+                for (int j = i + 1; j < txtvalue.Length; j++)
+                {
+                    if (char.ToLower(txtvalue[i]) == char.ToLower(txtvalue[j]))
+                    {
+                        Console.WriteLine($"Duplicate Characters : {txtvalue[i]}");
+
+                    }
+
+                }
+
+            }
+
+
+            //  Console.WriteLine($"Duplicate Characters : {dup}");
+
+        
+
+
+
+        }
+        #endregion
+
+        #region Find the first non-repeating character.
+
+        public static void FirstNonRepeting(string txtvalue)
+        {
+
+            for (int i = 0; i < txtvalue.Length; i++)
+            {
+                bool isduplicate = false;
+                for (int j = 0; j < txtvalue.Length; j++)
+                {
+                    if (1 != j && char.ToLower(txtvalue[i]) == char.ToLower(txtvalue[j]))
+                    {
+                        isduplicate = true;
+                        break;
+                    
+                    }
+                
+                
+                }
+                if (!isduplicate)
+                {
+                    Console.WriteLine($"First non repeting character is : {txtvalue[i]}");
+                
+                }
+            
+            
+            }
+
+        }
+
+        #endregion
 
         public static void Main(string[] args)
         {
+            Console.WriteLine("Enter a string:");
+            string txtvalue = Console.ReadLine() ?? string.Empty;
+            
+            Reverse(txtvalue);
+            FindDuplicate(txtvalue);
+            FirstNonRepeting(txtvalue);
 
+
+            //var employee1 = new EmployeeDto(1, "Atul", 50000);
+
+            //var employee2 = employee1 with
+            //{
+            //    Salary = 60000
+            //};
+            //Employee employee1 = new Employee("John", 30);
             //List<int> numbers = new List<int>
             //{
             //    10, 20, 30, 40, 50
@@ -26,10 +130,10 @@ namespace Interview_prep_8777
 
             //     int value = numbers[3];
 
-            int[] arr = { 1, 2, 3, 4, 5 };
-            int target = 6;
-            var result = TwoSum(arr, target);
-            Console.WriteLine($"Indices: {result[0]} , {result[1]} : {arr[result[0]]} +  {arr[result[1]]} = {target}");
+            //int[] arr = { 1, 2, 3, 4, 5 };
+            //int target = 6;
+            //var result = TwoSum(arr, target);
+            //Console.WriteLine($"Indices: {result[0]} , {result[1]} : {arr[result[0]]} +  {arr[result[1]]} = {target}");
         }
 
         public static int[] TwoSum(int[] nums, int target)
